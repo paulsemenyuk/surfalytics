e2ca81bec4ae063eecd55d94137212a8ea537402
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-11-22 14:17:54 -0700

    5.8 SQL queries executed by their employees

---
+++ 01_sql/5_Interview_Questions/5_8_IBM db2 Product Analytics/SQL_queries_executed_by_their_employees.sql
@@ -0,0 +1,18 @@
+WITH employee_queries AS (
+  SELECT
+    e.employee_id,
+    COALESCE(COUNT(DISTINCT q.query_id), 0) AS unique_queries
+  FROM employees AS e
+  LEFT JOIN queries AS q
+    ON e.employee_id = q.employee_id
+      AND q.query_starttime >= '2023-07-01T00:00:00Z'
+      AND q.query_starttime < '2023-10-01T00:00:00Z'
+  GROUP BY e.employee_id
+)
+
+SELECT
+  unique_queries,
+  COUNT(employee_id) AS employee_count
+FROM employee_queries
+GROUP BY unique_queries
+ORDER BY unique_queries;
