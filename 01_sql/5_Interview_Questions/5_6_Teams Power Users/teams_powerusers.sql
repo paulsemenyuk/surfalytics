f9f293238792239a97ffdc49d7d37f9dab2054de
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-11-26 06:22:52 -0700

    5.6 Updated solution

---
+++ 01_sql/5_Interview_Questions/5_6_Teams Power Users/teams_powerusers.sql
@@ -0,0 +1,9 @@
+SELECT
+  sender_id,
+  COUNT(message_id) AS count_messages
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date) = '8'
+  AND EXTRACT(YEAR FROM sent_date) = '2022'
+GROUP BY sender_id
+ORDER BY count_messages DESC
+LIMIT 2;
