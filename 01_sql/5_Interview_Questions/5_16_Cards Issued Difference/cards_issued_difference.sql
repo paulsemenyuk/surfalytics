1f4c7350dd1ad6c85fe87b6c7554fab4832b5b60
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-06 10:30:01 -0700

    5.16 Sulotion

---
+++ 01_sql/5_Interview_Questions/5_16_Cards Issued Difference/cards_issued_difference.sql
@@ -0,0 +1,11 @@
+-- Solution
+
+SELECT
+    card_name,
+    MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM
+    monthly_cards_issued
+GROUP BY
+    card_name
+ORDER BY
+    difference DESC;
