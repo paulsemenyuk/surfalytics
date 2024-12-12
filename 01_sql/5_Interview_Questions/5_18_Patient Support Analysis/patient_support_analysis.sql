01ceceb25f4cb00ad6b5d551d5ac67fd8d9ce641
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-12 07:47:36 -0700

    5.18 Solution update

---
+++ 01_sql/5_Interview_Questions/5_18_Patient Support Analysis/patient_support_analysis.sql
@@ -0,0 +1,44 @@
+-- My Soultion
+
+WITH count_cte AS (
+  SELECT
+    policy_holder_id,
+    COUNT(case_id) AS calls
+  FROM
+    callers
+  WHERE
+    call_duration_secs > 0
+  GROUP BY
+    policy_holder_id
+  ORDER BY
+    policy_holder_id
+)
+SELECT
+  COUNT(policy_holder_id) AS count
+FROM
+  count_cte
+WHERE
+  calls >= 3;
+
+-- Alternative with HAVING
+
+WITH count_cte AS (
+  SELECT
+    policy_holder_id,
+    COUNT(case_id) AS calls
+  FROM
+    callers
+  WHERE
+    call_duration_secs > 0
+  GROUP BY
+    policy_holder_id
+    HAVING
+        calls >= 3
+  ORDER BY
+    policy_holder_id
+)
+SELECT
+  COUNT(policy_holder_id) AS count
+FROM
+  count_cte
+;
