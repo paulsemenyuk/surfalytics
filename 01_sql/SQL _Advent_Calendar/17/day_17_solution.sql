cd9ba940c2b2ad02611b1a51cbf623902007f0c0
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-19 08:19:32 -0700

    SQL Advent Day 17 Solution

--- 01_sql/SQL _Advent_Calendar/17/day_17_solution.sql
+++ 01_sql/SQL _Advent_Calendar/17/day_17_solution.sql
@@ -6,4 +6,19 @@ select
 from
     grinch_pranks
 where
-    difficulty is not 'Beginner';
+    difficulty is not 'Beginner'
+order by
+    prank_name desc, location desc;
+
+
+-- Solution 2
+
+select
+    prank_name,
+    location
+from
+    grinch_pranks
+where
+    difficulty = 'Advanced' or difficulty = 'Expert'
+order by
+    prank_name desc, location desc;
