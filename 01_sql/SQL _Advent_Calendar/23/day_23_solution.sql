60c648ab3a8575996d0c2eab20cb2a7d9c045a1e
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-23 18:33:15 -0700

    SQL Advent Day 23 Solution

--- 01_sql/SQL _Advent_Calendar/23/day_23_solution.sql
+++ 01_sql/SQL _Advent_Calendar/23/day_23_solution.sql
@@ -6,3 +6,14 @@ select
     OVER (ORDER BY day_of_month), 0) as daily_weight_change
 from
     grinch_weight_log;
+
+
+-- Solution 2 using self-join
+
+SELECT
+    today.day_of_month,
+    coalesce((today.weight - yesterday.weight),0) AS daily_change
+FROM grinch_weight_log AS today
+LEFT JOIN grinch_weight_log AS yesterday -- you can also remove 'COALESCE' and use INNER JOIN insted, but lose day 1
+    ON today.day_of_month = yesterday.day_of_month + 1
+ORDER BY today.day_of_month;
