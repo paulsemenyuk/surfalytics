0daaf90b8a66120c04f959bd89486b4ef799ffaa
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-25 13:21:12 -0700

    SQL Advent Day 24 Updated Solution

--- 01_sql/SQL _Advent_Calendar/24/day_24_solution.sql
+++ 01_sql/SQL _Advent_Calendar/24/day_24_solution.sql
@@ -1,4 +1,4 @@
--- Solution
+-- Solution with Window Function
 
 select
     delivery_date,
@@ -7,3 +7,28 @@ select
     ) as gifts_delivered
 from
    deliveries;
+
+-- Solution with self-join
+
+SELECT
+    d1.delivery_date,
+    d1.gifts_delivered,
+    SUM(d2.gifts_delivered) AS cumulative_gifts
+FROM deliveries d1
+JOIN deliveries d2
+    ON d2.delivery_date <= d1.delivery_date
+GROUP BY d1.delivery_date, d1.gifts_delivered
+ORDER BY d1.delivery_date;
+
+-- Solution with with Correlated Subquery
+
+SELECT
+    d1.delivery_date,
+    d1.gifts_delivered,
+    (
+        SELECT SUM(d2.gifts_delivered)
+        FROM deliveries d2
+        WHERE d2.delivery_date <= d1.delivery_date
+    ) AS cumulative_gifts
+FROM deliveries d1
+ORDER BY d1.delivery_date;
