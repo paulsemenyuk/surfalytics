64fc3fafa25c1a024d8a3128b57eba40bc3ea57d
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-27 11:27:33 -0700

    5.23 Task

--- 01_sql/5_Interview_Questions/5_23_Top Three Salaries/top_three_salaries.sql
+++ 01_sql/5_Interview_Questions/5_23_Top Three Salaries/top_three_salaries.sql
@@ -22,3 +22,29 @@ ORDER BY
    d.department_name,
    rs.salary DESC,
   rs.name;
+
+  -- Solution with Subquery
+
+  SELECT
+    d.department_name,
+    e.name,
+    e.salary
+FROM (
+    SELECT
+        employee_id,
+        name,
+        salary,
+        department_id,
+        RANK() OVER (
+            PARTITION BY department_id
+            ORDER BY salary DESC, name ASC
+        ) AS salary_rank
+    FROM employee
+) AS ranked
+JOIN department d ON ranked.department_id = d.department_id
+JOIN employee e   ON ranked.employee_id = e.employee_id
+WHERE salary_rank <= 3
+ORDER BY
+    d.department_name ASC,
+    salary DESC,
+    name ASC;
