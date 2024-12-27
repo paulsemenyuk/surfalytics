aeb077b84bcd68df949a65229819f33de0356bf1
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-27 11:22:19 -0700

    5.22 Solution

--- 01_sql/5_Interview_Questions/5_22_Sending vs. Opening Snaps/sending_vs_opening_snaps.sql
+++ 01_sql/5_Interview_Questions/5_22_Sending vs. Opening Snaps/sending_vs_opening_snaps.sql
@@ -1,4 +1,4 @@
--- Solution
+-- Solution usung CTE and CASE
 
 WITH cte as (
   SELECT
@@ -17,3 +17,20 @@ SELECT
   ROUND(open_sum / NULLIF(sent_sum + open_sum, 0) * 100.0, 2) as open_perc
 FROM cte
 ORDER BY age_bucket;
+
+
+-- Solution usung FILTER clause (only PostgreSQL and SQLite)
+
+SELECT
+  ab.age_bucket,
+  ROUND(100.0 *
+    SUM(a.time_spent) FILTER (WHERE a.activity_type = 'send')/
+    SUM(a.time_spent),2) AS send_perc,
+  ROUND(100.0 *
+    SUM(a.time_spent) FILTER (WHERE a.activity_type = 'open')/
+    SUM(a.time_spent),2) AS open_perc
+FROM activities
+INNER JOIN age_breakdown AS ab
+  ON a.user_id = ab.user_id
+WHERE a.activity_type IN ('send', 'open')
+GROUP BY ab.age_bucket;
