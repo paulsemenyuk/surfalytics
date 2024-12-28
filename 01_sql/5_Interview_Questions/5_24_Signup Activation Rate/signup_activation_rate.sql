5f1d4452798d5b67dc5b08a017a8c2d6082c28d5
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-28 10:17:24 -0700

    5.24 Task

---
+++ 01_sql/5_Interview_Questions/5_24_Signup Activation Rate/signup_activation_rate.sql
@@ -0,0 +1,26 @@
+-- Solution
+
+SELECT
+  ROUND(
+  COUNT(texts.email_id)::DECIMAL
+    /COUNT(DISTINCT emails.email_id),2)
+  AS activation_rate
+FROM emails
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id
+  AND texts.signup_action = 'Confirmed';
+
+  -- Solution with CTE
+
+WITH cte AS (
+  SELECT
+    COUNT(DISTINCT emails.user_id) AS total_users,
+    COUNT(texts.text_id) AS conf_users
+  FROM emails
+  LEFT JOIN texts
+    ON emails.email_id = texts.email_id
+  WHERE texts.signup_action = 'Confirmed'
+)
+SELECT
+  ROUND(conf_users::DECIMAL / NULLIF(total_users, 0), 2) AS confirm_rate
+FROM cte;
