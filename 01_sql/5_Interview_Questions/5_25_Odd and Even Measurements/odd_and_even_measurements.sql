f18d44f9b9606f01e1a6a4c3d0ab6c930719e73f
pavlosemeniuk <paulsemenyuk@gmail.com>
2024-12-30 09:12:27 -0700

    5.25 Solution Update

--- 01_sql/5_Interview_Questions/5_25_Odd and Even Measurements/odd_and_even_measurements.sql
+++ 01_sql/5_Interview_Questions/5_25_Odd and Even Measurements/odd_and_even_measurements.sql
@@ -1,4 +1,4 @@
--- Solution with CASE claus
+-- Solution with CASE clause
 
 WITH ranked_measurements AS (
   SELECT
@@ -14,5 +14,24 @@ SELECT
   measurement_day,
   SUM(CASE WHEN measurement_num % 2 = 1 THEN measurement_value ELSE 0 END) AS sum_odd,
   SUM(CASE WHEN measurement_num % 2 = 0 THEN measurement_value ELSE 0 END) AS sum_even
+FROM ranked_measurements
+GROUP BY measurement_day;
+
+-- Solution with FILTER function
+
+WITH ranked_measurements AS (
+  SELECT
+    CAST(measurement_time AS DATE) AS measurement_day,
+    measurement_value,
+    ROW_NUMBER() OVER (
+      PARTITION BY CAST(measurement_time AS DATE)
+      ORDER BY measurement_time) AS measurement_num
+  FROM measurements
+)
+
+SELECT
+  measurement_day,
+  SUM(measurement_value) FILTER (WHERE measurement_num % 2 != 0) AS odd_sum,
+  SUM(measurement_value) FILTER (WHERE measurement_num % 2 = 0) AS even_sum
 FROM ranked_measurements
 GROUP BY measurement_day;
