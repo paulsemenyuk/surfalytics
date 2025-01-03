232330a8fe14cdaa27461e2362a3132b4ccc8520
pavlosemeniuk <paulsemenyuk@gmail.com>
2025-01-02 19:03:23 -0700

    5.27 Solution

--- 01_sql/5_Interview_Questions/5_27_Compressed Mode/compressed_mode.sql
+++ 01_sql/5_Interview_Questions/5_27_Compressed Mode/compressed_mode.sql
@@ -1,3 +1,12 @@
 -- Sokution
 
 -- Solution from ChatGPT
+
+SELECT
+    item_count AS mode
+FROM items_per_order
+WHERE order_occurrences = (
+    SELECT MAX(order_occurrences)
+    FROM items_per_order
+)
+ORDER BY item_count ASC;
